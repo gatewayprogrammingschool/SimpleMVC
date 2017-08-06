d55c2130414d049b23af4e2c25f667c51e8ede8b
Sharp Ninja <ninja@thesharp.ninja>
2017-08-06 15:07:42 -0500

    Converted to Paket from Nuget

--- SimpleMVC/GPS.SimpleMVC.Tests/ModelImplementations/TestModel.cs
+++ SimpleMVC/GPS.SimpleMVC.Tests/ModelImplementations/TestModel.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
-using GenericExtensionMethods;
 using GPS.SimpleDI;
 using GPS.SimpleMVC.Models;
 using GPS.SimpleMVC.Tests.AdapterImplementations;
