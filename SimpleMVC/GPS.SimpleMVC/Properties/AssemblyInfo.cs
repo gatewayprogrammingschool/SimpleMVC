c8df17c3b8eb00d77e2dd00fe947bbea2acf01fe
Sharp Ninja <ninja@thesharp.ninja>
2017-08-06 22:06:48 -0500

    Paket

--- SimpleMVC/GPS.SimpleMVC/Properties/AssemblyInfo.cs
+++ SimpleMVC/GPS.SimpleMVC/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GPS.SimpleMVC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GPS SimpleMVC")]
+[assembly: AssemblyDescription("Simple MVC Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gateway Programming School, Inc.")]
 [assembly: AssemblyProduct("GPS.SimpleMVC")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Gateway Programming School, Inc. 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
